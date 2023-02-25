9f0dc76fa0686ae57b15d31e0dfc0e1f7da0c4b3
DanteMilten <milten47@yahoo.com>
2023-02-25 21:57:18 +0300

    67

---
+++ 67/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 67: Напишите программу, которая будет принимать на вход число и 
+// возвращать сумму его цифр.
+// 453 -> 12
+// 45 -> 9
+
+Console.Clear();
+Console.Write("Введите число: ");
+int m = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"{m}->{SumNumbers(m)}");
+
+int SumNumbers(int number)
+{
+    if (number == 0)
+        return 0;
+    return (number % 10 + SumNumbers(number / 10));
+}
